eba4fb77eaeaf4c905564c085f96144f34b8082f
rozeringunes <rozeringunes65@gmail.com>
2024-05-16 23:20:43 +0300

    core package

---
+++ core/src/main/java/com/turkcell/crm/core/model/AccessToken.java
@@ -0,0 +1,13 @@
+package com.turkcell.crm.core.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+public class AccessToken {
+    private String token;
+    private Date expiration;
+}
