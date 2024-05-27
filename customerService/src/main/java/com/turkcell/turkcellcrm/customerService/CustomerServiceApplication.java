2ca5f1e274fae792d2f5aeb1bc1a9afe867c215c
rozeringunes <rozeringunes65@gmail.com>
2024-05-27 16:02:37 +0300

    @EnableSecurity annotation added

--- customerService/src/main/java/com/turkcell/turkcellcrm/customerService/CustomerServiceApplication.java
+++ customerService/src/main/java/com/turkcell/turkcellcrm/customerService/CustomerServiceApplication.java
@@ -1,9 +1,11 @@
 package com.turkcell.turkcellcrm.customerService;
 
+import com.turkcell.crm.core.annotation.EnableSecurity;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableSecurity
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
